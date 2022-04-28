764fae4fb85e6123d1c61774ac03c9c8dafa0dd1
Rafael Castro <ruevko@gmail.com>
2022-04-28 18:53:17 -0500

    added renderer

---
+++ render.r
@@ -0,0 +1,16 @@
+# check pkgs availability
+for (pkg in c("rmarkdown", "pagedown", "postcards")) {
+   if (pkg %in% installed.packages()[ , "Package"]) {
+      message("{", pkg, "} available")
+   } else {
+      install.packages(c(pkg), quiet = TRUE)
+      if (! pkg %in% installed.packages()[ , "Package"]) {
+         stop("{", pkg, "} installation failed")
+      }
+      message("{", pkg, "} installed")
+   }
+}
+# render home page
+rmarkdown::render("index.rmd", output_dir = "docs")
+# render resume page
+rmarkdown::render("resume.rmd", output_dir = "docs")
